f0c77ca2018c5b93952b4e14381bf14d8b4fc8d2
dtaylor0 <drewtaylorm@gmail.com>
2019-03-09 23:26:11 -0500

    ree

--- app/src/main/java/com/example/mealmatch/MatchScreenViewModel.kt
+++ app/src/main/java/com/example/mealmatch/MatchScreenViewModel.kt
@@ -8,10 +8,11 @@ import com.google.firebase.database.FirebaseDatabase
 class MatchScreenViewModel: ViewModel() {
     fun findMatches(person: Person): ArrayList<Person>{
         //TODO: use algorithm to find matches
-        val ref=FirebaseDatabase.getInstance().getReference("people")
+
+        /*val ref=FirebaseDatabase.getInstance().getReference("people")
         val personID=(ref.push().key).toString()
         person.id=personID
-        ref.child("people").child(personID).setValue(person)
+        ref.child("people").child(personID).setValue(person)*/
         return arrayListOf(Person("Drew",false,"email",Locations.BDH,1200),
             Person("Veronica",false,"phone",Locations.WOODYS,1500))
     }
